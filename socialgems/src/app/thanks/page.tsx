50458cfe4dc9a8e037a8ca1cb6ad2b25ba237c56
elishamugenyi <elishamugenyi5@gmail.com>
2025-03-12 13:49:36 +0300

    added thank you icon.

--- socialgems/src/app/thanks/page.tsx
+++ socialgems/src/app/thanks/page.tsx
@@ -4,12 +4,13 @@ import Footer  from "../components/footer";
 
 export default function ThankYouPage() {
     return (
-      <div className="min-h-screen flex flex-col bg-gray-100">
+      <div className="min-h-screen flex flex-col">
         <Navbar />
-        <div className="bg-white p-4 flex-grow flex  items-center justify-center">
+        <div className="bg-white p-4 flex-grow flex  items-center justify-center text-green-600">
           <div className="bg-white p-8 rounded-lg shadow-2xl max-w-md w-full text-center">
             <h1 className="text-3xl font-bold text-green mb-4">Thank You!</h1>
-            <p className="text-gold">
+            <>✅</>
+            <p className="text-l font-bold mt-2">
                 Your message has been sent successfully. We'll get back to you soon.
             </p>
           </div>
